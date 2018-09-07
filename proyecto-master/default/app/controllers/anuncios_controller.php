e378d8ce9022cec5db89278334c8579633e267ca
Raw Software Tech <rawsoftwaretech@gmail.com>
2018-09-07 17:54:30 +0200

    20180907_baseJ
    ..

---
+++ proyecto-master/default/app/controllers/anuncios_controller.php
@@ -0,0 +1,35 @@
+<?php
+
+class AnunciosController extends AppController {
+
+    public $template = "sexiindex";
+
+    protected function before_filter() {
+        parent::before_filter();
+    }
+
+    public function index() {
+        
+    }
+
+    public function login() {
+        
+    }
+
+    public function iniciosesion() {
+        
+    }
+
+    public function formulario($action = NULL, $method = 'POST', $attrs = NULL) {
+        
+    }
+    
+    public function ciudades(){
+        
+    }
+    
+    public function personalbanner($action = NULL, $method = 'POST', $attrs = NULL){
+        
+    }
+
+}
