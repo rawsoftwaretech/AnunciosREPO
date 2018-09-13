5e8424380319d734788d49be0b270dd74a9d4d11
Raw Software  Tech <rawsoftwaretech@gmail.com>
2018-09-13 20:54:08 +0200

    20180913

---
+++ proyecto-master/default/app/controllers/usuarios_controller.php
@@ -0,0 +1,25 @@
+<?php
+
+class UsuariosController extends  AppController{
+    
+    protected function before_filter() {
+        parent::before_filter();
+    }
+    
+    
+    public function index(){
+        
+    }
+    
+    public function registrar(){
+        
+    }
+    
+    public function iniciarsesion(){
+        
+    }
+    
+    public function cliente(){
+        
+    }
+}
